9ac51642f22621c6071f37b48f6c9e78a74ae899
foram <foram.s@techroversolutions.com>
2026-01-23 22:42:20 +0530

    fix: update logo source in Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -125,7 +125,7 @@ export const Footer = () => {
             {/* SME Oceanways Logo */}
             <div style={styles.logoWrapperv2}>
               <img
-                src="/image/logos.png"
+                src="/image/logo.png"
                 alt="SME Oceanways Logo"
                 style={styles.logo}
               />
